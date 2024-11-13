a9d6b1060fe90020154d636ac125d9f3de556cce
Rijad Comor <136588733+RijadCo@users.noreply.github.com>
2024-11-13 12:55:23 +0100

    fixed sln

--- GQI-GetMetadata_1/GetMetadata.cs
+++ GQI-GetMetadata_1/GetMetadata.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using Newtonsoft.Json;
     using Skyline.DataMiner.Analytics.GenericInterface;
     using Skyline.DataMiner.Net.Helper;
